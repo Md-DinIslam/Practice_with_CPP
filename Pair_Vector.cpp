5514c641f50404c51467af1aad51c980a6c421fb
Muhammad Dinislam <108214424+Md-DinIslam@users.noreply.github.com>
2023-05-04 15:04:49 +0600

    Add files via upload

--- Pair_Vector.cpp
+++ Pair_Vector.cpp
@@ -1,43 +1,135 @@
 #include <bits/stdc++.h>
 using namespace std;
-void printVector(vector<int> &arr)
-{
-    cout << "Size: " << arr.size() << endl;
-    for (int i = 0; i < arr.size(); i++)
-    {
-        cout << arr[i] << " "<<endl;
+
+// Speed
+#define Code ios_base::sync_with_stdio(false);
+#define By cin.tie(NULL);
+#define Din cout.tie(NULL);
+
+// Aliases
+using ll = long long;
+using lld = long double;
+using ull = unsigned long long;
+
+// TypeDEf
+typedef pair<ll, ll> pll;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef vector<pll> vpll;
+typedef vector<string> vs;
+typedef vector<vll> mat;
+typedef unordered_map<ll, ll> umpll;
+typedef unordered_map<int, int> umpi;
+typedef map<ll, ll> mpll;
+
+// Macros
+#define ff first
+#define ss second
+#define pb push_back
+#define pp pop_back
+#define mp make_pair
+#define fl(i, n) for (int i = 0; i < n; i++)
+#define rep(i,a, n) for (int i = a; i <= n; i++)
+#define rfl(i, m, n) for (int i = n; i >= m; i--)
+#define py cout << "YES\n";
+#define pm cout << "-1\n";
+#define pn cout << "NO\n";
+#define pf cout << "FIRST\n";
+#define nn cout << "\n";
+#define bitcount __builtin_popcountll
+#define endl '\n'
+#define all(v) v.begin(), v.end()
+#define rall(v) v.end(), v.begin()
+
+//DEBUG
+void __print(int x) {cerr << x << " ";}
+void __print(long x) {cerr << x << " ";}
+void __print(long long x) {cerr << x << " ";}
+void __print(unsigned x) {cerr << x << " ";}
+void __print(unsigned long x) {cerr << x << " ";}
+void __print(unsigned long long x) {cerr << x << " ";}
+void __print(float x) {cerr << x << " ";}
+void __print(double x) {cerr << x << " ";}
+void __print(long double x) {cerr << x << " ";}
+void __print(char x) {cerr << '\'' << x << '\'';}
+void __print(const char *x) {cerr << '\"' << x << '\"';}
+void __print(const string &x) {cerr << '\"' << x << '\"';}
+void __print(bool x) {cerr << (x ? "true" : "false");}
+
+template<typename T, typename V>
+void __print(const pair<T, V> &x) {cerr << '{'; __print(x.first); cerr << ','; __print(x.second); cerr << '}';}
+template<typename T>
+void __print(const T &x) {int f = 0; cerr << '{'; for (auto &i : x) cerr << (f++ ? "," : ""), __print(i); cerr << "}";}
+void _print() {cerr << "]\n";}
+template <typename T, typename... V>
+void _print(T t, V... v) {__print(t); if (sizeof...(v)) cerr << ", "; _print(v...);}
+#ifndef ONLINE_JUDGE
+#define debug(x...) cerr << "[" << #x << "] = ["; _print(x)
+#else
+#define debug(x...)
+#endif
+
+// Constants
+const lld PI = 3.141592653589793238;
+const ll INF = 1e18 + 9;
+const ll mod = 1e9 + 7;
+
+bool sortB(const pair<ll, ll> &a, const pair<ll, ll> &b) {
+    return (a.second < b.second);
+}
+void Print(vector<pair<ll, ll>> &pr) {
+    for (auto &x : pr) {
+        cout << x.first << "  " << x.second; nn
     }
 }
+void solve()
+{
+    int n;
+    cin >> n;
+    // vector<ll> v(n);
+    // fl(i, n) cin >> v[i];
+    // sort(v.begin(), v.end());
+    vector<pair<ll, ll>> prv(n);
+    vector<pair<ll, ll>> prv2;
+    for (int i = 0; i < n; ++i) {
+        int x;
+        cin >> x;
+        // cin >> prv[i].ff;
+        // prv[i].ss = i;
+        // prv[i].first = x;
+        // prv[i].second = i;
+        prv[i] = {x, i};
+        prv2.push_back({x, i});
+    }
+    // for(int i = 0;i < n; ++i){
+    //     int x;
+    //     cin>>x;
+    //     prv2.push_back({x,i});
+    // }
+    sort(prv.begin(), prv.end(), sortB);
+    sort(prv2.begin(), prv2.end(), sortB);
+    __print(prv);
+    // Print(prv);
+    Print(prv2);
+}
+// Main
 int main()
 {
-    int N;
-    cin >> N;
-    vector<int> arr[N];
-    for (int i = 0; i < N; i++)
-    {
-        int n;
-        cin >> n;
-        for (int j = 0; j < n; j++)
-        {
-            int x;
-            cin >> x;
-            arr[i].push_back(x);
-        }
-    }
-    for (int i = 0; i < N; i++)
-    {
-        printVector(arr[i]);
-    }
-    /* vector<pair<int,int>> p;
-    printVector(p);
-    int n; cin>>n;
-    for(int i=0;i<n;i++)
-    {
-        int x,y;
-        cin>>x>>y;
-        p.push_back({x,y}); // make_pair or '{}' same...
-    }
-    swap(p[0],p[n-1]);
-    printVector(p); */
+    Code By Din
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    freopen("Error.txt", "w", stderr);
+#endif
+    clock_t z = clock();
+    // ll t; cin >> t;
+    // while(t--) solve();
+    solve();
+    // fl(i,t) //Kickstart
+    // {
+    //     cout<<"Case #"<<i+1<<": ";
+    //     solve(); nn
+    // }
+    cerr << "\nRun Time : " << (((double)(clock() - z)) / CLOCKS_PER_SEC);
     return 0;
 }
